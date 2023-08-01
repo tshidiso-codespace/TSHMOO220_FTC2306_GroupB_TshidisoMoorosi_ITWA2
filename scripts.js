112efa35d93e25e9fb594a9af80445eaf1c42af9
tshidiso-codespace <tshidisomoorosi@gmail.com>
2023-08-02 00:13:32 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+/* Challenge 1*/
+
+/* This is the firstname of the user */
+
+const user = 'John'
+
+
+/* This is the lastname of the user */ 
+
+const surname = 'Smith'
+
+
+console.log(user; surname;)
+
+
+
+/* Challenge 2 */
+
+/* (c) ACME Inc. 2010 */
+
+/** This is the date that a user created their account */
+
+const date = '10/07/2014'
+
+console(date)
+
+
+/* Challenge 3 */
+
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn(Security scan starting)
+
+/* It is important to let user know when they can close the page  */
+
+console.info('Please wait for scan to complete before closing the browser.')
